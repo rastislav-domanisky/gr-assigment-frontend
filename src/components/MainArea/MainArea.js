b8d590693e5f3795060486a8fdb299244d4bdc7a
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-10 17:30:48 +0100

    added dog image

--- src/components/MainArea/MainArea.js
+++ src/components/MainArea/MainArea.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./MainArea.scss";
+import Form from './Form/Form';
+
+import dogImg from '../../assets/dog.png';
 
 const MainArea = () => {
     return (
         <main className="MainArea">
-
+            <Form />
+            <div className="dogImage">
+                <img alt="Dog" src={dogImg} />
+            </div>
         </main>
     );
 }
