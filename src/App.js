937f631d9238d1729e34578112db4a238d8a5616
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-10 15:21:04 +0100

    Added MainArea (empty)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import MainArea from './components/MainArea/MainArea';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainArea />
     </div>
   );
 }
---
+++ src/components/MainArea/MainArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./MainArea.scss";
+
+const MainArea = () => {
+    return (
+        <main className="MainArea">
+
+        </main>
+    );
+}
+
+export default MainArea;
