7b1b1a19c38a6739461319d7579ab9dbdb08b71a
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-10 16:23:33 +0100

    Added Horizontal Separator

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header/Header';
 import MainArea from './components/MainArea/MainArea';
 import Footer from './components/Footer/Footer';
+import HorizontalSeparator from './components/HorizontalSeparator/HorizontalSeparator';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainArea />
+      <HorizontalSeparator />
       <Footer />
     </div>
   );
---
+++ src/components/HorizontalSeparator/HorizontalSeparator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HorizontalSeparator.scss';
+
+const HorizontalSeparator = () => {
+    return (
+        <div className="HorizontalSeparator">
+            <div className="hLine" />
+        </div>
+    );
+}
+
+export default HorizontalSeparator;
