30ae098d1d23d73de1c77b54189902c7809e2d6a
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-11 20:09:38 +0100

    fixed amount validator

--- src/models/AmountValidator.js
+++ src/models/AmountValidator.js
@@ -1,4 +1,7 @@
 const amountValidator = (value) => {
+    if(typeof value != "string") {
+        return true;
+    }
     value= value.trim()
     if(!isNaN(value) && !isNaN(parseFloat(value))) {
         return true;
