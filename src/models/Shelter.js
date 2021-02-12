52a423d852e805be71f274154a34d2a3634e0bea
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-12 12:30:32 +0100

    create POST data function

--- src/models/Shelter.js
+++ src/models/Shelter.js
@@ -1,10 +1,27 @@
+import axios from 'axios';
+
 class Shelter {
     getShelters = async () => {
-        const xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", "https://frontend-assignment-api.goodrequest.com/api/v1/shelters", false);
-        xmlHttp.send(null);
-        return JSON.parse(xmlHttp.response)["shelters"];
+        try {
+            const xmlHttp = new XMLHttpRequest();
+            xmlHttp.open("GET", "https://frontend-assignment-api.goodrequest.com/api/v1/shelters", false);
+            xmlHttp.send(null);
+            return JSON.parse(xmlHttp.response)["shelters"];
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
     }
+
+    createContrib = async (data) => {
+        return await axios.post("https://frontend-assignment-api.goodrequest.com/api/v1/shelters/contribute", data).then((response) => {
+            return response.data["messages"][0];
+        }).catch((error) => {
+            console.log(error);
+            return null;
+        });
+    }
+
 }
 
 export default Shelter;
